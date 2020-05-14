0a876ce9cc47d8ec9dc8db1f4e884245e5ba5582
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-05-14 11:06:39 -0300

    Updates

---
+++ precache-manifest.7466a31cbb7df2927f5635e9160dfdd3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119f049045a7323ec2652a5bef8fa5b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1856bb8bff5b9f06575e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "31d5f74a8776339a710d",
+    "url": "/static/css/main.28282198.chunk.css"
+  },
+  {
+    "revision": "1856bb8bff5b9f06575e",
+    "url": "/static/js/2.257dc008.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.257dc008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d5f74a8776339a710d",
+    "url": "/static/js/main.ccfaea85.chunk.js"
+  },
+  {
+    "revision": "3b2d70c4f1353768e237",
+    "url": "/static/js/runtime-main.749938c7.js"
+  },
+  {
+    "revision": "4d28fcf2585a1cd35f79790d0bad6f9a",
+    "url": "/static/media/administrador.4d28fcf2.png"
+  }
+]);
