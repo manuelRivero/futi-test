759104031b53b728e242dd42e10e04d50bf55773
manuel rivero <manuel.enrique.r.v@gmail.com>
2020-05-14 11:21:44 -0300

    Updates

---
+++ precache-manifest.c9d88b4e25205d0908d614ae542817c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f48fb69b300c20e03b20f97ba39dc9",
+    "url": "/futi-test/index.html"
+  },
+  {
+    "revision": "1856bb8bff5b9f06575e",
+    "url": "/futi-test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cbcefa46599772006497",
+    "url": "/futi-test/static/css/main.28282198.chunk.css"
+  },
+  {
+    "revision": "1856bb8bff5b9f06575e",
+    "url": "/futi-test/static/js/2.257dc008.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/futi-test/static/js/2.257dc008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbcefa46599772006497",
+    "url": "/futi-test/static/js/main.f682d07e.chunk.js"
+  },
+  {
+    "revision": "b225563ad55056bfd073",
+    "url": "/futi-test/static/js/runtime-main.61b4ae3e.js"
+  },
+  {
+    "revision": "4d28fcf2585a1cd35f79790d0bad6f9a",
+    "url": "/futi-test/static/media/administrador.4d28fcf2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7466a31cbb7df2927f5635e9160dfdd3.js"
+  "/futi-test/precache-manifest.c9d88b4e25205d0908d614ae542817c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/futi-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
